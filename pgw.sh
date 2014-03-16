7aabb0e5a527fa880d025bcac5dfcdd205ad967a
Thomas Dwyer <github@tomd.tel>
2014-03-15 23:57:50 -1000

    NEW Feature: If only output type (-clip|-window|-screen|-stdout) then display numbered list and ask user for number of object to decrypt. NEW Feature: If Tree output is longer then lines in term pipe to less. Improvement: validation() now in easier to follow case in asec statements. Other: Bottom of file defines 16color ANSI colors with via tput

--- pgw.sh
+++ pgw.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#   GpgWallet       GPLv3              v10.5
+#   GpgWallet       GPLv3              v10.6
 #   Thomas Dwyer    <devel@tomd.tel>   http://tomd.tel/
-DBUG=           # If DBUG not Null display parsed args
 HELP="
 Usage ${0} [-l (search-term)] [-e] [-d] [-clip] [-screen] [-window #]
                 [-s example.com] [-k username (-p password)] [-v filename]
@@ -16,10 +15,16 @@ Usage ${0} [-l (search-term)] [-e] [-d] [-clip] [-screen] [-window #]
 -window #       :Send to stdin of gnu-screen window Number
 -s  str         :Search wallet with: -d, -k, -v and/or string found in name
 -t  str         :Same as Search but view your wallet in a colored tree format
-" ;GPG=$(which gpg)
-SCREEN=$(which screen)
+" ; [[ ${1} == "--help" || ${1} == "-h" ]] && (echo "${HELP}" ; exit 0)
+GPG=$(which gpg) ;[[ ${?} -gt 0 ]] && (echo "Install gpg (GnuPG) >=2.0")
+SCREEN=$(which screen 2>/dev/null) 
 XCLIP=$(which xclip)
-[[ -z $GNUPGHOME ]] && export GNUPGHOME="$HOME/.gnupg" ;WAL="$GNUPGHOME/wallet"
+XCS=( ["1"]="primary" ["2"]="secondary" ["3"]="clipboard" )
+FIND=$(which find)
+TREE=$(which tree)
+[[ -z $PAGER ]] && PAGER="less"
+[[ $PAGER == "less" ]] && LESS="--RAW-CONTROL-CHARS"
+[[ -z $GNUPGHOME ]] && GNUPGHOME="$HOME/.gnupg" ;WAL="$GNUPGHOME/wallet"
 KEYID="$(cat "${WAL}/.KEYID")"
 # - - - List, Encrypt, or Decrypt objects - - - #
 main() {
@@ -29,7 +34,7 @@ main() {
         ;;
         tree)
             if [[ $(expr 4 + $(treeUI | wc -l)) -lt $(tput lines) ]] ;then
-                treeUI ;else treeUI | less ;fi
+                treeUI ;else treeUI -C | $PAGER ;fi
         ;;
         encrypt)
             [[ ! -d ${wdir}  ]] && mkdir -p ${wdir}
@@ -38,21 +43,17 @@ main() {
                 echo -n "${val}" | ${GPG} -e -r "${KEYID}" > ${wdir}/${obj}
         ;;
         decrypt)
-            if [[ "${dst}" == "stdout" ]] ;then
-                ${GPG} --batch --quiet -d ${wdir}/${obj} ;exit 0 ;fi
+            local plaintext=$(${GPG} --batch --quiet -d ${wdir}/${obj})
+            if [[ "${dst}" == "stdout" ]] ;then echo -n ${plaintext} ;exit 0 ;fi
             case "${dst}" in
                 clip)
-                    s=( ["1"]="primary" ["2"]="secondary" ["3"]="clipboard" )
-                    ${GPG} --batch --quiet -d ${wdir}/${obj} | \
-                        ${XCLIP} -selection ${s[${sel}]} -in
+                    echo -n ${plaintext} | ${XCLIP} -selection ${XCS[${sel}]} -in
                 ;;
                 screen)
-                    ${SCREEN} -S $STY \
-                    -X register . $(${GPG} --batch --quiet -d ${wdir}/${obj})
+                    ${SCREEN} -S $STY -X register . ${plaintext}
                 ;;
                 window)
-                    ${SCREEN} -S $STY -p "${num}" \
-                    -X stuff $(${GPG} --batch --quiet -d ${wdir}/${obj})
+                    ${SCREEN} -S $STY -p "${sel}" -X stuff ${plaintext}
             esac
     esac ; exit ${?}
 }
@@ -60,11 +61,19 @@ main() {
 parse_args() {
     flags='' ;for arg in ${@} ;do
         case "${flag}" in
-            -s)
-                cmd='find' ; str="${arg}"
+            -d)
+                dom="${arg}"
             ;;
-            -t)
-                cmd='tree' ; str="${arg}"
+            -k)
+                typ='keyring' ; obj="${arg}"
+            ;;
+            -v)
+                val="${arg}"
+            ;;
+            -f)
+                typ='vault' ; val="${arg}"
+                obj="$(echo ${arg} |sed 's/\//\n/g' |tail -n1)"
+                [[ $(echo $obj |rev |cut -d '.' -f 1) != 'gpg' ]] && obj+=".gpg"
             ;;
             -e)
                 cmd='encrypt'
@@ -79,69 +88,119 @@ parse_args() {
                 cmd='decrypt' ; dst='screen'
             ;;
             -window)
-                cmd='decrypt' ; dst='window' ; num="${arg}"
+                cmd='decrypt' ; dst='window' ; sel="${arg}"
             ;;
-            -d)
-                dom="${arg}"
+            -s)
+                cmd='find' ; str="${arg}"
             ;;
-            -k)
-                typ='keyring' ; obj="${arg}"
-            ;;
-            -v)
-                val="${arg}"
-            ;;
-            -f)
-                typ='vault' ; val="${arg}"
-                obj="$(echo ${arg} |sed 's/\//\n/g' |tail -n1)"
-                [[ $(echo $obj |rev |cut -d '.' -f 1) != 'gpg' ]] && obj+=".gpg"
+            -t)
+                cmd='tree' ; str="${arg}"
         esac ; flag="${arg}"
     done ; wdir="${WAL}/${dom}/${typ}" ; validate
 }
 # - - - Check for errors - - - #
 validate() {
-    [[ ! -z $DBUG ]] && echo "ARGS*$ted:$srv:$wdir:$obj:$val:$dst:$num"
-    [[ ! "1 2 3" =~ ${sel}  ]] && sel='1'
-    if [[ ${cmd} != "tree" && ${cmd} != "find" ]] ;then
-        if [[ -z ${KEYID} ]] ;then echo "Put GPG uid in: ${WAL}/.KEYID" ; exit 1
-        elif [[ -z ${cmd} || -z ${dom} || -z ${typ} || -z ${obj} ]] ;then
+    case ${cmd} in
+        encrypt)
+            if [[ -z ${dom} || -z ${typ} || -z ${obj} ]] ;then
+                echo "${HELP}" ; exit 201
+            fi
+            case ${typ} in
+                keyring)
+                    if [[ -z ${val} ]] ;then
+                        read -sp "Enter passwd: " val
+                        echo; read -sp "Re-enter passwd: " v
+                        if [[ ${val} != ${v} || -z ${val} ]] ;then
+                            echo 'Passwords did not match!' ; exit 203
+                        fi
+                    fi
+                ;;
+                vault)
+                    if [[ -z ${val} ]] ;then echo ${HELP} ;exit 202 ;fi
+            esac
+        ;;       
+        decrypt)
+            if [[ ! "stdout clip screen window" =~ ${dst} ]] ;then
+                echo "${HELP}" ; exit 101
+            fi
+            case ${dst} in
+                clip)
+                    [[ -z ${XCS[${sel}]} ]] && sel=1
+                ;;
+                window)
+                    $(expr ${sel} + 1 1>/dev/null 2>&1)
+                    if [[ ! ${?} -eq 0 ]] ;then
+                        echo "Destination Window number: ${sel} :is invalid"
+                        echo "${HELP}" ; exit 102
+                    fi
+                ;;
+                screen)
+                    [[ -z $STY ]] && echo ' - No $STY -' ;echo ${HELP} ;exit 103
+            esac
+            [[ -z ${dom} || -z ${typ} || -z ${obj} ]] && selectList
+        ;;
+        tree)
+            [[ "-d -k -v -f ZZZ" =~ "${str}" ]] && str=''
+        ;;
+        *)
             echo "${HELP}" ; exit 255
-        elif [[ ${dst} == 'window' && -z ${num} ]] ;then
-            echo "Missing Window Number" ; exit 128
-        elif [[ ${cmd} == 'encrypt' && ${typ} == 'vault' && ! -f ${val} ]] ;then
-            echo "File not found" ; exit 128
-        elif [[ ${cmd} == "encrypt" && ${typ} == "keyring" && -z ${val} ]] ;then
-            read -sp "Enter passwd: " val ;echo; read -sp "Re-enter passwd: " v
-            if [[ ${val} != ${v} || -z ${val} ]] ;then
-                echo 'Passwords did not match!' ; exit 128 ; fi
-        fi
-    else [[ "-d -k -v -p ZZZ" =~ "${str}" ]] && str='' ;fi ; main
+    esac ; main
 }
 # - - - tree command view - - - #
 treeUI() {
-    cd ${WAL} ;a="tree --noreport --prune -C"
-    if [[ -z ${dom} ]] ;then
-        if [[ -z ${str} ]] ;then
-            [[ -z ${typ} ]] && $a || $a ./*/${typ}
-        else
-            [[ -z ${typ} ]] && $a -P "*${str}*" || $a -P "*${str}*" ./*/${typ}
-        fi
-    else
-        [[ -z ${str} ]] && $a ${dom}/${typ} || $a -P "*${str}*" ${dom}/${typ}
-    fi
+    cd ${WAL} ;a="--noreport --prune" ;s="*${str}*"
+    for p in ${@} ;do a+=" ${p}" ;done
+    [[ ${typ} == "vault" ]] && s+=".gpg"
+    [[ ${typ} == "keyring" ]] && a+=" -I *.gpg"
+    a+=" -P "
+    ${TREE} ${a} "${s}" ${dom}
 }
-# - - - find command view - - - #
-findUI() {
-    cd ${WAL} ;a="find -mindepth"
-    if [[ -z ${dom} ]] ;then
-        if [[ -z ${str} ]] ;then
-            [[ -z ${typ} ]] && $a 3 || $a 3 -path "./*/${typ}/*"
+# - - - Gen index - - - #
+genIndex() {
+    index=$(find $(ls --color=never) -type f)
+    [[ ! -z ${dom} ]] && index=$(echo $index |sed 's/ /\n/g' |grep -E "${dom}//*")
+}
+# - - - Select from list - - - #
+selectList() {
+    cd ${WAL} ;genIndex
+    genList
+    read -p "$(echo -ne ${WHT}) Enter choice #$(echo -ne ${clr}) " choice 
+    dom=$(echo ${index} |cut -d ' ' -f ${choice} |cut -d '/' -f 1)
+    typ=$(echo ${index} |cut -d ' ' -f ${choice} |cut -d '/' -f 2)
+    obj=$(echo -n ${index} |cut -d ' ' -f ${choice} |cut -d '/' -f 3)
+    wdir="${WAL}/${dom}/${typ}" ;main 
+}
+# - - - Gen select list - - - #
+genList() {
+    cd ${WAL} ;start_color=3 ;LN=1
+    for line in $(echo $index) ;do
+        # Set color
+        if [[ -z ${t} ]] ;then
+            t='togle'
+            local c1=$(tput bold; tput setaf ${start_color})
+            local c2=$(tput bold; tput setaf $(expr $start_color + 1))
+            local c3=$(tput bold; tput setaf $(expr $start_color + 2))
         else
-            [[ -z ${typ} ]] && $a 3 -name "*${str}*" || \
-                $a 3 -path "./*/${typ}/*" -name "*${str}*"
+            t=''
+            local c1=$(tput setaf ${start_color})
+            local c2=$(tput setaf $(expr $start_color + 1))
+            local c3=$(tput setaf $(expr $start_color + 2))
         fi
-    else
-        cd ${dom}/${typ} ; [[ -z ${str} ]] && $a 1 || $a 1 -name "*${str}*"
-    fi
+        # align columns
+        dom="$(echo ${line} |cut -d '/' -f 1)"
+        typ="$(echo ${line} |cut -d '/' -f 2)"
+        obj="$(echo ${line} |cut -d '/' -f 3)"
+        ln="" ;n=$(expr 4 - $(echo -n ${LN} |wc -c))
+        while [[ $n -gt 0 ]] ;do n=$(expr $n - 1) ;ln+=" " ;done ;ln+="${LN} - "
+        n=$(expr 16 - $(echo -n ${dom} |wc -c))
+        while [[ $n -gt 0 ]] ;do n=$(expr $n - 1) ;dom+=" " ;done
+        n=$(expr 12 - $(echo -n ${typ} |wc -c))
+        while [[ $n -gt 0 ]] ;do n=$(expr $n - 1) ;typ+=" " ;done
+        echo -ne "${c3}${ln}${clr}" ;LN=$(expr $LN + 1)
+        echo -ne "${c1}${dom}${clr}"
+        echo -ne "${c2}${typ}${clr}"
+        echo -e "${c3}${obj}${clr}"
+    done ;
 }
 #
 # The idea is to transparently use Git to provide:
@@ -180,5 +239,45 @@ gitInit() {
     if [[ -f ${WAL}/.gitignore ]] ;then
         local todo = true ;fi
 }
+# - - - Color me encrypted - - - #
+BlD=$(tput blink;tput bold;tput setaf 0)
+RrD=$(tput blink;tput bold;tput setaf 1)
+GrN=$(tput blink;tput bold;tput setaf 2)
+YeL=$(tput blink;tput bold;tput setaf 3)
+BlU=$(tput blink;tput bold;tput setaf 4)
+MaG=$(tput blink;tput bold;tput setaf 5)
+CyN=$(tput blink;tput bold;tput setaf 6)
+WhT=$(tput blink;tput bold;tput setaf 7)
+bLk=$(tput blink;tput setaf 0)
+rEd=$(tput blink;tput setaf 1)
+gRn=$(tput blink;tput setaf 2)
+yEl=$(tput blink;tput setaf 3)
+bLu=$(tput blink;tput setaf 4)
+mAg=$(tput blink;tput setaf 5)
+cYn=$(tput blink;tput setaf 6)
+wHt=$(tput blink;tput setaf 7)
+BLD=$(tput bold;tput setaf 0)
+RED=$(tput bold;tput setaf 1)
+GRN=$(tput bold;tput setaf 2)
+YEL=$(tput bold;tput setaf 3)
+BLU=$(tput bold;tput setaf 4)
+MAG=$(tput bold;tput setaf 5)
+CYN=$(tput bold;tput setaf 6)
+WHT=$(tput bold;tput setaf 7)
+blk=$(tput setaf 0)
+red=$(tput setaf 1)
+grn=$(tput setaf 2)
+yel=$(tput setaf 3)
+blu=$(tput setaf 4)
+mag=$(tput setaf 5)
+cyn=$(tput setaf 6)
+wht=$(tput setaf 7)
+nrm=$(tput sgr0)
+clr='\033[m' #GNU less command likes this better then echo -ne $(tput sgr0)
+Nl="s/^/$(tput setaf 7)/g" # sed -e ${Nl} -e ${nL} Will color 'nl' number lists
+nL="s/\t/$(echo -ne '\033[m')\t/g"
+#
+# - - - RUN - - - #
 parse_args ${@} ZZZ #The ZZZ is flag+value for-loop padding
-# /* vim: set ts=4 sw=4 tw=80 et :*/
+exit 1
+# vim: set ts=4 sw=4 tw=80 et
